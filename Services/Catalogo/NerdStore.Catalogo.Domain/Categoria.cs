65a069a4c8aaec81325880a9518ef0c76e4d9e9c
Leandro <leandrodbdias@gmail.com>
2020-06-25 18:37:57 -0300

    implementando camada de repositório e dbcontex com entity framework

--- Services/Catalogo/NerdStore.Catalogo.Domain/Categoria.cs
+++ Services/Catalogo/NerdStore.Catalogo.Domain/Categoria.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
 using NerdStore.Core.DomainObjects;
 
 namespace NerdStore.Catalogo.Domain
 {
     public class Categoria : Entity
     {
+        protected Categoria() { }
+
         public Categoria(string nome, int codigo)
         {
             Nome = nome;
@@ -14,6 +17,9 @@ namespace NerdStore.Catalogo.Domain
 
         public string Nome { get; private set; }
         public int Codigo { get; private set; }
+
+        // Entity Framework
+        public ICollection<Produto> Produtos { get; set; }
 
         public override string ToString() => $"{Nome} - {Codigo}";
 
